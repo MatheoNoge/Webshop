478fdf7e71bd1255c23ba83bbaadf8ebb162afb4
MatheoNoge <116355590+MatheoNoge@users.noreply.github.com>
2023-02-23 20:17:16 +0100

    nästan klart

---
+++ products.js
@@ -0,0 +1,138 @@
+const products = [
+  {
+    id: 0,
+    name: "(C64) Spy vs Spy",
+    price: 299.99,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/C64 Spy vs Spy.jpg",
+  },
+  {
+    id: 1,
+    name: "(C64) Batman",
+    price: 249.99,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/C64 Batman.jpg",
+  },
+  {
+    id: 2,
+    name: "(C64)CaliforniaGames",
+    price: 199.99,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/C64 California Games.jpg",
+  },
+  {
+    id: 3,
+    name: "(SMD) Sonic the Hedgehog 2",
+    price: 259.99,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/SMD Sanic2.jpg",
+  },
+  {
+    id: 4,
+    name: "(SMD) Super Monaco",
+    price: 299.99,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/SMD Super Monaco.jpg",
+  },
+  {
+    id: 5,
+    name: "(SMD) Fifa 96",
+    price: 399.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/SMD Fifa96.jpg",
+  },
+  {
+    id: 6,
+    name: "(PS) Crash Bandicoot",
+    price: 599.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/PS Crash Bandicoot.jpg",
+  },
+  {
+    id: 7,
+    name: "(PS) Wipeout",
+    price: 399.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/PS Wipeout.jpg",
+  },
+  {
+    id: 8,
+    name: "(PS) Formula One 2000",
+    price: 399.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/PS Formula one.jpg",
+  },
+  {
+    id: 9,
+    name: "(Atari) Pac-Man",
+    price: 499.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/Atari Päkmän.jpg",
+  },
+  {
+    id: 10,
+    name: "(Atari) Video Pinball",
+    price: 299.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/Atari Pinball.jpg",
+  },
+  {
+    id: 11,
+    name: "(Atari) Asteroids",
+    price: 299.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/Atari Asteroids.jpg",
+  },
+  {
+    id: 12,
+    name: "(VP) Basketball/Bowling",
+    price: 199.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/PV Basket.jpg",
+  },
+  {
+    id: 13,
+    name: "(VP) Golf",
+    price: 199.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/PV Golf.jpg",
+  },
+  {
+    id: 14,
+    name: "(PV) Loony Baloon",
+    price: 199.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./images/PV Loony Baloon.jpg",
+  },
+  
+];
